6c2f78d94e14d0e20dafa9a0f38816b93ec0921f
Nathan <nathaniellucero03@gmail.com>
2026-03-02 07:45:11 +0800

    published committed

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext"; // adjust path if needed
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </AuthProvider>,
 );
